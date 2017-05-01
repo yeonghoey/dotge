ce7865dbb1ee6dfefb0eaad6defa336d60c6b249
Yeongho Kim <yeonghoey@gmail.com>
2017-05-01 14:17:54 +0900

    Add Cover

---
+++ Assets/Scripts/Blinker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blinker : MonoBehaviour {
+
+	public float speed;
+
+	private Text text;
+	private string message;
+
+	void Start () {
+		text = GetComponent<Text> ();
+		message = text.text;
+	}
+
+	void Update () {
+		float v = Mathf.PingPong (Time.time * speed, 1.0f);
+		bool show = Mathf.Round (v) > 0;
+		text.text = show ? message : "";
+	}
+}
