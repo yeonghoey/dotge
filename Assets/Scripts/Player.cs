5cd8ae9b17def2e446033e2733a36ddf4812e491
Yeongho Kim <yeonghoey@gmail.com>
2017-05-07 23:48:47 +0900

    Write TestHelperWindow

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,37 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour {
+namespace Dotge
+{
+    public class Player : MonoBehaviour {
 
-    public float speed;
+        public float speed;
 
-    private Animator animator;
-    private Vector2 direction;
+        private Animator animator;
+        private Vector2 direction;
 
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-        direction = Vector2.zero;
-    }
-
-    void Update()
-    {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
-        direction = new Vector2(h, v).normalized;
-    }
-
-    void FixedUpdate()
-    {
-        transform.Translate(direction * speed * Time.fixedDeltaTime);
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Shot"))
+        void Start()
         {
-            animator.SetTrigger("Die");
+            animator = GetComponent<Animator>();
+            direction = Vector2.zero;
+        }
+
+        void Update()
+        {
+            float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxis("Horizontal");
+            direction = new Vector2(h, v).normalized;
+        }
+
+        void FixedUpdate()
+        {
+            transform.Translate(direction * speed * Time.fixedDeltaTime);
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+#if UNITY_EDITOR
+            if (UnityEditor.EditorPrefs.GetBool("Dotge.invincible", false))
+            {
+                return;
+            }
+#endif
+            if (other.CompareTag("Shot"))
+            {
+                animator.SetTrigger("Die");
+            }
         }
     }
 }
---
+++ Assets/Scripts/TestHelperWindow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Dotge
+{
+    public class TestHelperWindow : EditorWindow
+    {
+        bool invincible = false;
+
+        [MenuItem("Custom/Test Helper")]
+        public static void Open()
+        {
+            GetWindow<TestHelperWindow>();
+        }
+
+        void OnGUI()
+        {
+            PrefBool("invincible", ref invincible);
+        }
+
+        void PrefBool(string name, ref bool value)
+        {
+            var key = "Dotge." + name;
+            value = EditorPrefs.GetBool(key);
+            invincible = EditorGUILayout.Toggle(key, value);
+            EditorPrefs.SetBool(key, invincible);
+        }
+    }
+}
