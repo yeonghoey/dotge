608c5d3dc675b324a24c6f0be658e52a6aa72d93
Yeongho Kim <yeonghoey@gmail.com>
2017-05-07 13:35:58 +0900

    Reformat Blinker

--- Assets/Scripts/Blinker.cs
+++ Assets/Scripts/Blinker.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class Blinker : MonoBehaviour {
-
+public class Blinker : MonoBehaviour
+{
 	public float speed;
 
 	private Text text;
 	private string message;
 
-	void Start () {
-		text = GetComponent<Text> ();
+	void Start()
+  {
+		text = GetComponent<Text>();
 		message = text.text;
 	}
 
-	void Update () {
-		float v = Mathf.PingPong (Time.time * speed, 1.0f);
-		bool show = Mathf.Round (v) > 0;
+	void Update()
+  {
+		float v = Mathf.PingPong(Time.time * speed, 1.0f);
+		bool show = Mathf.Round(v) > 0;
 		text.text = show ? message : "";
 	}
 }
