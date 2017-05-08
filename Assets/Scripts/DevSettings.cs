dee273478c1fa233910a705bbd484bcae81ca023
Yeongho Kim <yeonghoey@gmail.com>
2017-05-08 22:47:57 +0900

    Working on DevSettings

---
+++ Assets/Scripts/DevSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+public static class DevSettings {
+    // public static bool Invincible
+    // {
+    //     get { retrun PlayerPrefs.GetInt("invincible"); }
+    // }
+
+    public static bool Invincible
+    {
+        get { return PlayerPrefs.GetInt("invincible") != 0; }
+    }
+}
+#endif
